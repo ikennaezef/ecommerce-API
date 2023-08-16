ad7ad8c4f9fd861e0be33c096b97d4729a71b33c
Ikenna <iykee0333@gmail.com>
2023-08-16 10:23:30 +0100

    image upload and deletion

---
+++ src/utils/imageUpload.ts
@@ -0,0 +1,75 @@
+import { NextFunction, Response } from "express";
+import multer from "multer";
+import config from "../config/firebase";
+import { initializeApp } from "firebase/app";
+import {
+	getStorage,
+	deleteObject,
+	ref,
+	getDownloadURL,
+	uploadBytesResumable,
+} from "firebase/storage";
+import { ExpressRequest } from "../interfaces";
+
+initializeApp(config.firebaseConfig);
+
+const storage = getStorage();
+
+const upload = multer({ storage: multer.memoryStorage() });
+
+const uploadImage = async (req: ExpressRequest) => {
+	try {
+		const { picturePath } = req.body;
+		if (!picturePath) {
+			throw Error("Picture path is required!");
+		}
+
+		if (!req.file) {
+			throw Error("No file uploaded!");
+		}
+
+		const dateTime = new Date().toISOString();
+
+		const storageRef = ref(
+			storage,
+			`ecommerce_files/products/${req?.file?.originalname} ${dateTime}`
+		);
+
+		// Create file metadata including the content type
+		const metadata = {
+			contentType: req?.file?.mimetype,
+		};
+
+		// Upload file to bucket storage
+		const snapshot = await uploadBytesResumable(
+			storageRef,
+			req?.file?.buffer!,
+			metadata
+		);
+
+		// Grab the public url
+		const downloadURL = await getDownloadURL(snapshot.ref);
+
+		if (!downloadURL) {
+			throw Error("An error occured");
+		}
+
+		req.image = downloadURL;
+
+		return;
+	} catch (error: any) {
+		throw Error(error.message);
+	}
+};
+
+const deleteImage = async (imageURL: string) => {
+	try {
+		const imageRef = ref(storage, imageURL);
+		await deleteObject(imageRef);
+		return;
+	} catch (error) {
+		throw Error("An error occured while deleting!");
+	}
+};
+
+export { uploadImage, upload, deleteImage };
