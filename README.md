b8e5f43a5d763d4d7cf978ad484cd6b46cf290e1
Ikenna <iykee0333@gmail.com>
2023-08-23 11:53:07 +0100

    tidy up entry file && README

---
+++ README.md
@@ -0,0 +1,7 @@
+# E-Commerce API
+
+Backend for an E-commerce application built with Node.js, Express and Typescript. It utilizes MongoDB as a database.
+
+## Documentation
+
+The Postman documentation for this API can be found [here](https://documenter.getpostman.com/view/28630550/2s9Y5VUPgB)
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,19 @@
 import express, { Request, Response } from "express";
+import path from "path";
 import cors from "cors";
 import dotenv from "dotenv";
+import helmet from "helmet";
 
 import { connectDb } from "./db/connect";
+import {
+	addressRouter,
+	authRouter,
+	orderRouter,
+	productRouter,
+	reviewRouter,
+	userRouter,
+} from "./routes";
+import { notFound } from "./middleware/not-found";
 
 dotenv.config();
 
@@ -10,17 +21,30 @@ const PORT = process.env.PORT! || 3001;
 
 const app = express();
 app.use(express.json());
+app.use(express.static(__dirname));
 app.use(cors());
+app.use(helmet());
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
 
 app.get("/", (req: Request, res: Response) => {
-	res.send("<h1>Authentication API</h1>");
+	res.send("<h1>E-Commerce API</h1>");
 });
 
-// app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/addresses", addressRouter);
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/products", productRouter);
+app.use("/api/v1/reviews", reviewRouter);
+app.use("/api/v1/orders", orderRouter);
+
+app.use(notFound);
 
 const start = async () => {
 	try {
 		await connectDb(process.env.MONGO_URI!);
+
 		app.listen(PORT, () => console.log(`SERVER RUNNING ON PORT ${PORT}...`));
 	} catch (error) {
 		console.log("COULD NOT START SERVER", error);
